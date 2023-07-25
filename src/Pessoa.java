c18ed6df787ce79f6cc2227e1a4170afa6b25c9c
Rafael da Silva Muniz <rafaelmunizrsm@hotmail.com>
2023-07-25 09:35:47 -0300

    Exercício módulo 7 backend

---
+++ src/Pessoa.java
@@ -0,0 +1,48 @@
+import java.util.Date;
+
+/** Classe para registro dos dados das pessoas.
+ * @author : Rafael da Silva Muniz
+ * @since : 24/07/23
+ * @version : 1.0
+ */
+
+public class Pessoa {
+
+    public String nome;
+    private String endereco;
+    private String cpf;
+
+    private Date dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
